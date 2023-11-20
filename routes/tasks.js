fe5846c81fbfa27912c162578951a589d737164f
Azad Kamarbandi <akamarbandi@unitedprint.rdb>
2023-11-20 13:47:29 +0100

    added all functionality

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { 
-    getAllTasks, 
-    createTask, 
-    getTask, 
-    updateTask, 
-    deleteTask 
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
 } = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
